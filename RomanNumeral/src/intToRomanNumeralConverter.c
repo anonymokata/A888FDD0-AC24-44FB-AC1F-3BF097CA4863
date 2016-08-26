6fe008113a04e9108eaab5b5e46ce6b757ecdc44
anonymous <anonymous>
2016-08-26 11:24:10 -0400

    Refactoring into separate methods

--- RomanNumeral/src/intToRomanNumeralConverter.c
+++ RomanNumeral/src/intToRomanNumeralConverter.c
@@ -1,28 +1,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+int convertIntGreaterThanEqToOneHund(char *romanNumerals, int value, int *lenPtr){
+  int len = *lenPtr;
+  int subFromVal = 0;
+
+  if(value >= 1000){
+    romanNumerals[len] = 'M';
+    subFromVal = 1000;
+  } else if(value >= 500){
+    romanNumerals[len] = 'D';
+    subFromVal = 500;
+  } else if(value >= 100){
+    romanNumerals[len] = 'C';
+    subFromVal = 100;
+  }
+
+  len++;
+  return subFromVal;
+}
+
+int convertIntGreaterThanEqToFive(char *romanNumerals, int value, int *lenPtr){
+  int len = *lenPtr;
+  int subFromVal = 0;
+
+  if(value >= 50){
+    romanNumerals[len] = 'L';
+    subFromVal = 50;
+  } else if(value >= 10){
+    romanNumerals[len] = 'X';
+    subFromVal = 10;
+  } else if(value >= 5){
+    romanNumerals[len] = 'V';
+    subFromVal = 5;
+  }
+
+  len++;
+  return subFromVal;
+}
+
 void convertIntToRomanNumerals(char *romanNumerals, int value){
   int len = 0;
 
   while(value > 0){
-    if(value >= 1000){
-      romanNumerals[len] = 'M';
-      value -= 1000;
-    } else if(value >= 500){
-      romanNumerals[len] = 'D';
-      value -= 500;
-    } else if(value >= 100){
-      romanNumerals[len] = 'C';
-      value -= 100;
-    } else if(value >= 50){
-      romanNumerals[len] = 'L';
-      value -= 50;
-    } else if(value >= 10){
-      romanNumerals[len] = 'X';
-      value -= 10;
+    if(value >= 100){
+      value -= convertIntGreaterThanEqToOneHund(romanNumerals, value, &len);
     } else if(value >= 5){
-      romanNumerals[len] = 'V';
-      value -= 5;
+      value -= convertIntGreaterThanEqToFive(romanNumerals, value, &len);
     } else {
       romanNumerals[len] = 'I';
       value -= 1;
