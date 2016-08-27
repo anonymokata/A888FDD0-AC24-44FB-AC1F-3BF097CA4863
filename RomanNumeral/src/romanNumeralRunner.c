711f1dd1ebd3569113ef817e89b18cef02a4902c
anonymous <anonymous>
2016-08-27 12:01:44 -0400

    S

--- RomanNumeral/src/romanNumeralRunner.c
+++ RomanNumeral/src/romanNumeralRunner.c
@@ -12,7 +12,10 @@ int main(int argc, char *argv[]){
     char result[25];
     calculate(result, argv[1], argv[2], argv[3]);
     printf("Result: %s\n", result);
+    return 0;
   }
 
+  printf("\nUsage: romanNumeral [operation] [romanNumeral] [romanNumeral]\n");
+  printf("Operations: add, sub, mul, div\n\n");
   return 0;
 }
