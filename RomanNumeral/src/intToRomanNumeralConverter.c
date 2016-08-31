b8b96eac4f3f23d1bb676476bd808a14f9a43223
anonymous <anonymous>
2016-08-31 16:00:49 -0400

    Refactor int to roman numeral

--- RomanNumeral/src/intToRomanNumeralConverter.c
+++ RomanNumeral/src/intToRomanNumeralConverter.c
@@ -6,22 +6,21 @@ void convertIntToRomanNumerals(char *romanNumerals, int value){
   int numVals[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
   int len = 0;
 
+  int index = 0;
   while(value > 0){
-    int index = 0;
-    for(index; index < sizeof(numVals); index++){
-      if(value >= numVals[index]){
-	if(strlen(numerals[index]) > 1) {
-	  romanNumerals[len] = numerals[index][0];
-	  romanNumerals[len + 1] = numerals[index][1];
-	  len += 2;
-	} else {
-	  romanNumerals[len] = numerals[index][0];
-	  len++;
-	}
-
-	value = value - numVals[index];
-	break;
+    if(value >= numVals[index]){
+      if(strlen(numerals[index]) > 1) {
+	romanNumerals[len] = numerals[index][0];
+	romanNumerals[len + 1] = numerals[index][1];
+	len += 2;
+      } else {
+	romanNumerals[len] = numerals[index][0];
+	len++;
       }
+
+      value = value - numVals[index];
+    } else {
+      index++;
     }
   }
 
