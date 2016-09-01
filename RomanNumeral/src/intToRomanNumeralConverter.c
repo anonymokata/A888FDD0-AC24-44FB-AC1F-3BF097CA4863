cf5d53ffcf7f8aa3f4cbd59da86094cd8bc174d6
anonymous <anonymous>
2016-09-01 18:32:27 -0400

    renamed struct tuple to numeralValues

--- RomanNumeral/src/intToRomanNumeralConverter.c
+++ RomanNumeral/src/intToRomanNumeralConverter.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct tuple tuple;
+typedef struct numeralValues numeralValues;
 
-struct tuple {
+struct numeralValues {
   char *numeral;
   int value;
 };
 
 void convertIntToRomanNumerals(char *romanNumerals, int value){
-  tuple numerals[] = {{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}};
+  numeralValues numerals[] = {{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}};
 
   int index = 0;
   while(value > 0){
